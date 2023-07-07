f221738fafa239c5165a0279310dd551b694790c
chentao <978744151@qq.com>
2023-07-07 11:11:07 +0800

    first

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <AmisEditor id="editorName" :value="schema" @change="onChange" />
+  </div>
+</template>
+
+<script setup lang="js">
+import { createApp } from 'vue'
+import "amis/lib/themes/default.css";
+import "amis/lib/helper.css";
+import "amis/sdk/iconfont.css";
+import "amis-editor-core/lib/style.css";
+import "amis-ui/lib/themes/cxd.css";
+import {applyReactInVue, applyPureReactInVue} from 'veaury'
+import { Editor } from "amis-editor";
+const AmisEditor = applyPureReactInVue(Editor)
+const schema = {}
+const onChange = () =>{}
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+  }
+}
+</style>
